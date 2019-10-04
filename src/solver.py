bf3fafd24d5035f3477d5fa480d28144810e95cb
romanstrazanec <roman.strazanec007@gmail.com>
2019-10-04 17:01:13 +0200

    method solve with left fill

--- src/solver.py
+++ src/solver.py
@@ -17,3 +17,20 @@ class Solver:
 
     def __str__(self) -> str:
         return '\n'.join('|'.join(value_to_mark(elem) for elem in line) for line in self.solution)
+
+    def solve(self):
+        # iterate over lines, keeping its index to access self.solution
+        for line_index, line in enumerate(self.left):
+            col_index = 0  # keep column index to access self.solution
+
+            # iterate over line, where elem is the number written in line
+            for elem in line:
+                # fill solution with the corresponding number
+                for i in range(elem):
+                    self.solution[line_index][col_index + i] = 1
+                col_index += elem  # move col_index behind filled line
+
+                # if there is still a place, write an X mark behind the line
+                if col_index < self.size.width:
+                    self.solution[line_index][col_index] = -1
+                col_index += 1  # move index behind the X mark
