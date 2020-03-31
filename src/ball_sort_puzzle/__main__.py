92a2be5d7e5985e4e08ab271ee9821ead9c22b08
romanstrazanec <roman.strazanec007@gmail.com>
2020-03-31 23:32:52 +0200

    relative imports

--- src/ball_sort_puzzle/__main__.py
+++ src/ball_sort_puzzle/__main__.py
@@ -1,5 +1,5 @@
-from solver import BallSortPuzzleSolver
-from levels import Levels
+from .solver import BallSortPuzzleSolver
+from .levels import Levels
 
 b = BallSortPuzzleSolver(Levels.l2)
 b.solve()
