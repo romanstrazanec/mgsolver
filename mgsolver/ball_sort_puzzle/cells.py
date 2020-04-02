aa506d69022e1355028fa5ab72af60252aca40fa
romanstrazanec <roman.strazanec007@gmail.com>
2020-04-02 11:28:39 +0200

    src -> mgsolver

---
+++ mgsolver/ball_sort_puzzle/cells.py
@@ -0,0 +1,16 @@
+from .color import Color
+
+# level 5x
+cells = [
+    [Color.gray, Color.purple, Color.green, Color.orange],
+    [Color.red, Color.pink, Color.pink, Color.pink],
+    [Color.lightblue, Color.green, Color.purple, Color.green],
+    [Color.blue, Color.lightblue, Color.orange, Color.gray],
+    [Color.gray, Color.lightblue, Color.purple, Color.blue],
+    [Color.blue, Color.lightblue, Color.darkgreen, Color.pink],
+    [Color.orange, Color.red, Color.purple, Color.green],
+    [Color.red, Color.darkgreen, Color.gray, Color.darkgreen],
+    [Color.red, Color.blue, Color.orange, Color.darkgreen],
+    [],
+    []
+]
