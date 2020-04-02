b2ec10793dfaa2faba7f0f0ca9ec46aeae7636c3
romanstrazanec <roman.strazanec007@gmail.com>
2020-04-02 11:21:05 +0200

    sudoku solver

--- src/sudoku/solver.py
+++ src/sudoku/solver.py
@@ -1,94 +1,37 @@
-import numpy as np
-import pandas as pd
+from typing import List, Iterable
 
 
-def __next__(x, y):
-    return (0, y + 1) if x + 1 > 8 else (x + 1, y)
+class Sudoku:
 
+    def __init__(self, grid: List[List[int]]):
+        self.grid = [row[:] for row in grid]
 
-def __previous__(x, y):
-    return (8, y - 1) if x - 1 < 0 else (x - 1, y)
+    def row(self, y: int) -> List[int]:
+        return self.grid[y]
 
+    def col(self, x: int) -> Iterable[int]:
+        return (self.grid[i][x] for i in range(9))
 
-def solve(sudoku):
-    fixed_nums = sudoku > 0
-    x = y = 0
-    num = 1
-    
-    while x < 9 and y < 9:
-        if fixed_nums.iloc[y, x]:
-            x, y = __next__(x, y)
-            num = 1
-            continue
+    @staticmethod
+    def __in(n: int, i: int) -> bool:
+        div = (i // 3) * 3
+        return div < n < div + 3
 
-        if num > 9:
-            sudoku.iloc[y, x] = 0
-            x, y = __previous__(x, y)
-            while fixed_nums.iloc[y, x]:
-                x, y = __previous__(x, y)
-            if y < 0:
-                print("Sudoku has no solution!")
-                return None
-            num = sudoku.iloc[y, x] + 1
-            continue
+    def square(self, x: int, y: int) -> Iterable[int]:
+        return (self.grid[j][i] for j in range(9) for i in range(9) if self.__in(j, y) and self.__in(i, x))
 
-        row = np.array(sudoku.iloc[y, :])
-        col = np.array(sudoku.iloc[:, x])
-        square = np.array(sudoku.iloc[(y // 3) * 3:(y // 3) * 3 + 3, (x // 3) * 3:(x // 3) * 3 + 3])
-        
-        if num in row or num in col or num in square:
-            num += 1
-            continue
-        else:
-            sudoku.iloc[y, x] = num
-            x, y = __next__(x, y)
-            num = 1
-            continue
+    def possible(self, y: int, x: int, n: int) -> bool:
+        return n not in self.row(y) and n not in self.col(x) and n not in self.square(x, y)
 
-
-def solve_recursively(sudoku):
-    fixed_nums = sudoku > 0
-
-    def putnumber(x, y, num):
-        if num > 9:
-            sudoku.iloc[y, x] = 0
-            x, y = __previous__(x, y)
-            while fixed_nums.iloc[y, x]:
-                x, y = __previous__(x, y)
-                if y < 0:
-                    print("Sudoku has no solution!")
-                    return None
-            return putnumber(x - 1, y, sudoku.iloc[y, x - 1] + 1)
-
-        if y + 1 > 8 and x + 1 > 8:
-            return True
-
-        if fixed_nums.iloc[y, x]:
-            x, y = __next__(x, y)
-            return putnumber(x, y, 1)
-
-        row = np.array(sudoku.iloc[y, :])
-        col = np.array(sudoku.iloc[:, x])
-        square = np.array(sudoku.iloc[(y // 3)*3:(y // 3)*3 + 3, (x // 3)*3:(x // 3)*3 + 3])
-        
-        if num in row or num in col or num in square:
-            return putnumber(x, y, num + 1)
-        else:
-            sudoku.iloc[y, x] = num
-            x, y = __next__(x, y)
-            return putnumber(x, y, 1)
-
-    putnumber(0, 0, 1)
-
-
-def main():
-    print("Reading file...")
-    sudoku = pd.read_csv("sudoku.csv", dtype=int, header=None)
-    print(sudoku)
-    print("Solving sudoku...")
-    solve(sudoku)
-    print(sudoku)
-    
-
-if __name__ == "__main__":
-    main()
+    def solve(self) -> bool:
+        for y in range(9):
+            for x in range(9):
+                if self.grid[y][x] == 0:
+                    for n in range(1, 10):
+                        if self.possible(y, x, n):
+                            self.grid[y][x] = n
+                            if self.solve():
+                                return True
+                            self.grid[y][x] = 0
+                    return False
+        return True
